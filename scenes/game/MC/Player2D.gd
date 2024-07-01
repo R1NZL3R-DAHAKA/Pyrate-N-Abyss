fe502bea3aaee6d62212f6ce741ad9ce95dcd2df
SammaeL <66368599+R1NZL3R-DAHAKA@users.noreply.github.com>
2024-07-01 01:56:55 -0500

    Player2D_run+jump+collisions-fixed

---
+++ scenes/game/MC/Player2D.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+@onready var ap = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -200
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 600
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * SPEED
+	if direction !=0:
+		sprite.flip_h = (direction == -1)
+
+	update_animations(direction)
+	move_and_slide()
+	print(velocity)
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			ap.play("idle")
+		else:
+			ap.play("run")
+	else:
+		if velocity.y < 0:
+			ap.play("jump")
+		elif velocity.y > 0:
+			ap.play("fall")
+		else:
+			pass
