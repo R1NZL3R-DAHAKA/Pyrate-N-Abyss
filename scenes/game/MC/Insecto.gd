74f1c7e65ac552a859df8e75d9f98bfe75b02f70
Hacovo <corovel2004@gmail.com>
2024-07-05 15:14:40 -0500

    agregar enemigos

---
+++ scenes/game/MC/Insecto.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+var speed = -60.0
+var current_speed = 0.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var facing_right = false
+var dead = false
+var max_health = 1
+var health
+
+func _ready():
+	$AnimatedSprite2D.play("Fly") 
+	
+func _process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	if !$Node2D/RayCast2D.is_colliding() && is_on_floor():
+		flip()
+		
+	velocity.x = -speed
+	move_and_slide()
+	
+func flip():
+	facing_right = !facing_right
+	
+	scale.x = abs(scale.x) * -1
+	if facing_right:
+		speed = abs(speed)
+	else:
+		speed = abs(speed) * -1
+
+func _on_area_2d_area_entered(area):
+	if area.get_parent("Player2D"):
+		area.get_parent().die()
+
+func die():
+	queue_free()
